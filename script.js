a4967dbc3a0587d0bb5c56863c2e11ebbbd6b20a
milanajberhe <mjberhe@gmail.com>
2026-08-20 16:37:53 -0700

    Refactor script.js to manage project link interactions
    Removed sayHello function and added event listeners for project links.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function sayHello() {
-    alert("Hello!");
-}
+const projectLinks = document.querySelectorAll(
+    ".project-link-preview, .project-pdf"
+);
+
+projectLinks.forEach((element) => {
+
+    element.addEventListener("click", (event) => {
+        event.stopPropagation();
+    });
+
+});
